410850c664a7aab880b94614858ce62858252089
Alexander Seiler <seileralex@gmail.com>
2017-12-02 19:18:34 +0100

    Add solution of part 2 of day 2.

--- day_02/corruption_checksum.go
+++ day_02/corruption_checksum.go
@@ -10,13 +10,16 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	sum := 0
+	sum1 := 0
+	sum2 := 0
 	for scanner.Scan() {
 		rowElements := strings.Fields(scanner.Text())
 		min, max := getMinMax(rowElements)
-		sum += max - min
+		sum1 += max - min
+		sum2 += getDivisionResult(rowElements)
 	}
-	fmt.Println(sum)
+	fmt.Println("Solution of part 1:", sum1)
+	fmt.Println("solution of part 2:", sum2)
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input: ", err)
 	}
@@ -45,4 +48,29 @@ func getMinMax(row []string) (int, int) {
 		}
 	}
 	return min, max
+}
+
+func getDivisionResult(row []string) int {
+	var numbers []int
+	for _, elem := range row {
+		number, err := strconv.Atoi(elem)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		numbers = append(numbers, number)
+	}
+	for i, number1 := range numbers {
+		for _, number2 := range numbers[i+1:] {
+			sm := number1
+			bg := number2
+			if bg < sm {
+				sm, bg = bg, sm
+			}
+			if bg%sm == 0 {
+				return bg / sm
+			}
+		}
+	}
+	return 0
 }
