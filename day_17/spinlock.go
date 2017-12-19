bb03aadac81970abf09c9a7358e1b3c70fb28ce0
Alexander Seiler <seileralex@gmail.com>
2017-12-19 05:44:13 +0100

    Add solution for day 17.

--- day_17/spinlock.go
+++ day_17/spinlock.go
@@ -14,9 +14,10 @@ type Entry struct {
 }
 
 type Spinlock struct {
-	size    int
-	current *Entry
-	start   *Entry
+	size         int
+	current      *Entry
+	start        *Entry
+	currentIndex int
 }
 
 func (s *Spinlock) add(number int, steps int) {
@@ -27,6 +28,7 @@ func (s *Spinlock) add(number int, steps int) {
 		s.current = &entry
 		s.size = 1
 		s.start = &entry
+		s.currentIndex = 0
 		return
 	}
 	for i := 0; i < steps%s.size; i++ {
@@ -37,17 +39,45 @@ func (s *Spinlock) add(number int, steps int) {
 	s.current = &entry
 	entry.next = next
 	entry.previous = curr
+	s.currentIndex = (s.currentIndex + steps) % s.size
 	s.size++
+	s.currentIndex = (s.currentIndex + 1) % s.size
 	curr.next = &entry
 	next.previous = &entry
 }
 
-func (s *Spinlock) print() {
-	curr := s.current
-	for i := 0; i < s.size; i++ {
-		fmt.Printf("%d ", curr.number)
-		curr = curr.next
+func (s *Spinlock) simulateAdd(number int, steps int, afterZeroList []int) []int {
+	index := (s.currentIndex + steps) % s.size
+
+	if index == 0 {
+		afterZeroList = append(afterZeroList, number)
 	}
+	s.currentIndex = index
+	s.size++
+	s.currentIndex = (s.currentIndex + 1) % s.size
+
+	return afterZeroList
+
+}
+
+func (s *Spinlock) print() {
+	start := s.start
+	curr := start
+	if s.size == 0 {
+		return
+	}
+	for {
+		if curr == s.current {
+			fmt.Printf("(%d) ", curr.number)
+		} else {
+			fmt.Printf("%d ", curr.number)
+		}
+		curr = curr.next
+		if curr == start {
+			break
+		}
+	}
+	fmt.Println()
 }
 
 func main() {
@@ -57,21 +87,17 @@ func main() {
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input: ", err)
 		}
-		fmt.Println(input)
 		spinlock := Spinlock{}
 		spinlock.add(0, 0)
+
 		for i := 1; i <= 2017; i++ {
 			spinlock.add(i, input)
 		}
 		fmt.Println("Solution of part 1:", spinlock.current.next.number)
+		afterZeroList := []int{spinlock.start.next.number}
 		for i := 2018; i <= 50000000; i++ {
-			if i%1000000 == 0 {
-				fmt.Println(i)
-			}
-			spinlock.add(i, input)
+			afterZeroList = spinlock.simulateAdd(i, input, afterZeroList)
 		}
-		fmt.Println("Solution of part 2:", spinlock.start.next)
+		fmt.Println("Solution of part 2:", afterZeroList[len(afterZeroList)-1])
 	}
-	// fmt.Println("Solution of part 1:", sum1)
-	// fmt.Println("solution of part 2:", sum2)
 }
